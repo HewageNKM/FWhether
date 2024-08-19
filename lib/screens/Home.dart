9a32c3fe0c7348047a935667e6ebfb7cf7d1f7a0
Nadun Kawishika <107237482+HewageNKM@users.noreply.github.com>
2024-08-19 23:13:28 +0530

    Basic Weather function complete

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -41,23 +41,75 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Padding(
-          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-          child: Center(child: Text('FWeather')),
-        ),
-      ),
+      appBar: AppBar(),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(_weather?.cityName ?? "City Loading...."),
-            Lottie.asset(""),
-            Text(_weather?.main ?? "Conditions Loading...."),
-            Text(_weather?.temperature.toString() ?? "Temperature Loading....")
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.location_on_outlined,
+                    size: 30,
+                  ),
+                  Text(
+                    _weather?.cityName ?? "City Loading....",
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Lottie.asset(getWeatherAnimation(
+                      _weather?.main.toLowerCase() ?? "clear")),
+                  Text(
+                    _weather?.main ?? "Conditions Loading....",
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _weather?.temperature.toString() ?? "Temp Loading",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w500, fontSize: 25),
+                  ),
+                  const Text(
+                    " °C",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+String getWeatherAnimation(String condition) {
+  switch (condition) {
+    case "rain":
+      return "assets/rain.json";
+    case "snow":
+      return "assets/snow.json";
+    case "mist":
+      return "assets/mist.json";
+    case "thunderstorm":
+      return "assets/thunderstorm.json";
+    case "clouds":
+      return "assets/clouds.json";
+    case "clear":
+      return "assets/clear.json";
+    default:
+      return condition;
   }
 }
